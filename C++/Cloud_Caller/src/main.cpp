930c7e674b91f3089b37883364dbbc1c33a843b8
John Machado <johnmachado@Johns-MBP-2.attlocal.net>
2023-02-10 12:08:26 -0600

    Project template

---
+++ C++/Cloud_Caller/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main() {
+
+    std::cout << "Hello Cloud!" << std::endl;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Cloud-Python-to-Cpp
+
+WIP...
+
+GCP (Google Cloud Platform) Cloud Functions connection with C++ code. 
