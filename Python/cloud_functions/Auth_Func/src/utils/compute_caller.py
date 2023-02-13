abebf9a73ffdf6389c4252988fa8776af4a7e590
John Machado <johnmachado@Johns-MBP-2.attlocal.net>
2023-02-13 17:20:35 -0600

    Cleanup on compute func call. Test with int return on compute func

--- Python/cloud_functions/Auth_Func/src/utils/compute_caller.py
+++ Python/cloud_functions/Auth_Func/src/utils/compute_caller.py
@@ -6,23 +6,21 @@ import google.oauth2.id_token
 
 
 def call_compute_func():
+    """
+    Calls the Cloud Function "Compute_Func"
+    :return: *** Placeholder
+    """
 
     url = cf_url[0]
-
-    # response = requests.get(url)
-
-    # print(response.text)
-    # print(response.status_code)
-
-    # return response
-    # req = urllib.request.Request(endpoint)
 
     req = urllib.request.Request(url)
     auth_req = google.auth.transport.requests.Request()
     id_token = google.oauth2.id_token.fetch_id_token(auth_req, url)
-    # id_token = google.oauth2.id_token.fetch_id_token(auth_req, audience)
 
     req.add_header("Authorization", f"Bearer {id_token}")
     response = urllib.request.urlopen(req)
+
+    print("Datatype: ")
+    print(type(response.read()))
 
     return response.read()
