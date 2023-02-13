01627e17a44d0ba5c4ed3f8b8d6f757606571095
John Machado <johnmachado@Johns-MBP-2.attlocal.net>
2023-02-12 20:58:28 -0600

    Cloud Caller C++ code implemented.

--- C++/Cloud_Caller/src/main.cpp
+++ C++/Cloud_Caller/src/main.cpp
@@ -1,19 +1,43 @@
+#include <curl/curl.h>
 #include <iostream>
+#include <string>
 
 
 int main() {
 
-    std::cout << "Hello Cloud!" << std::endl;
+    std::cout << "Sending POST Request to Cloud Function" << std::endl;
 
-    // Send username + pw to Cloud Function endpoint. Serialize before sending.
-    // Json format probably easiest. 
-    // User + pw could be grabbed from environment variables.
+    CURL *curl;
+    CURLcode res;
+    long http_code = 0;
 
-    // Possible feature:
-    // Send "compute" as a boolean in the request
-    // If True, Python cloud function does number calculation
-    // If false, Python grabs data, returns data to C++, C++ does calculation amd
-    // Returns data to Cloud Function, auth will need to happen 2x
+    curl = curl_easy_init();
+    if(curl) {
+        
+        std::string json_data = "{\"email\":\"email here\", \"password\":\"password here\"}";
+        curl_easy_setopt(curl, CURLOPT_URL, "cloud function url here");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_data.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long)json_data.size());
+
+        struct curl_slist *headers = NULL;
+
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        res = curl_easy_perform(curl);
+
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+        else {
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+            std::cout << "\nHTTP status code: " << http_code << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
 
     return 0;
 }
